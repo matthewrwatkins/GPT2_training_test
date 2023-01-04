956bce4d854bf78c75e5f53b310c596706c50f2a
matthewrwatkins <110045781+matthewrwatkins@users.noreply.github.com>
2023-01-04 15:35:45 +0000

    Create training_function.py

---
+++ training_function.py
@@ -0,0 +1,51 @@
+def train(
+    dataset, model, tokenizer,
+    batch_size=16, epochs=5, lr=2e-5,
+    max_seq_len=400, warmup_steps=200,
+    gpt2_type="gpt2", output_dir=".", output_prefix="wreckgar",
+    test_mode=False,save_model_on_epoch=False,
+):
+    acc_steps = 100
+    device=torch.device("cuda")
+    model = model.cuda()
+    model.train()
+
+    optimizer = AdamW(model.parameters(), lr=lr)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=warmup_steps, num_training_steps=-1
+    )
+
+    train_dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    loss=0
+    accumulating_batch_count = 0
+    input_tensor = None
+
+    for epoch in range(epochs):
+
+        print(f"Training epoch {epoch}")
+        print(loss)
+        for idx, entry in tqdm(enumerate(train_dataloader)):
+            (input_tensor, carry_on, remainder) = pack_tensor(entry, input_tensor, 768)
+
+            if carry_on and idx != len(train_dataloader) - 1:
+                continue
+
+            input_tensor = input_tensor.to(device)
+            outputs = model(input_tensor, labels=input_tensor)
+            loss = outputs[0]
+            loss.backward()
+
+            if (accumulating_batch_count % batch_size) == 0:
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
+                model.zero_grad()
+
+            accumulating_batch_count += 1
+            input_tensor = None
+        if save_model_on_epoch:
+            torch.save(
+                model.state_dict(),
+                os.path.join(output_dir, f"{output_prefix}-{epoch}.pt"),
+            )
+    return model
