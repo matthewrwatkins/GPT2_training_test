9a14d2e042d8f7f602e1656256d2437a79f92446
matthewrwatkins <110045781+matthewrwatkins@users.noreply.github.com>
2023-01-04 15:32:47 +0000

    Create tokenizer_and_model.py

---
+++ tokenizer_and_model.py
@@ -0,0 +1,13 @@
+#Get the tokenizer and model
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+#Accumulated batch size (since GPT2 is so big)
+def pack_tensor(new_tensor, packed_tensor, max_seq_len):
+    if packed_tensor is None:
+        return new_tensor, True, None
+    if new_tensor.size()[1] + packed_tensor.size()[1] > max_seq_len:
+        return packed_tensor, False, new_tensor
+    else:
+        packed_tensor = torch.cat([new_tensor, packed_tensor[:, 1:]], dim=1)
+        return packed_tensor, True, None
